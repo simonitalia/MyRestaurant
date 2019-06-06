ee20cc5048fa1e75d7c37ba7d38ddb2247bbd4b6
Simon Italia <sitalia@magicaltomato.com>
2019-06-06 12:32:26 +0800

    Initial Commit

---
+++ MyRestaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyRestaurant
+//
+//  Created by Simon Italia on 6/6/19.
+//  Copyright © 2019 Magical Tomato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
